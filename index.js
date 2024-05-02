7aa133fb82aef9dc1adc48c84be2be2c794244f3
Efraim Marcatto <efraimmarcatto@gmail.com>
2024-05-02 06:47:00 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function calcularRank(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return {
+        nivel,
+        saldoVitorias
+    };
+}
+
+const resultado = calcularRank(60, 12);
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
